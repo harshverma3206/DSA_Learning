9db13a33140217aed7194172fd6ff9140a5bde20
Harsh <harshverma3206@gmail.com>
2026-08-06 07:47:53 +0530

    Practice 07

--- 02_ArrayManipulation/TwoSum.js
+++ 02_ArrayManipulation/TwoSum.js
@@ -5,7 +5,7 @@ for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
         if (nums[i] + nums[j] == target) {
             let ans = [i, j];
-            return ans;
+            return ans; 
         }
     }
 }
